2bd4067a7084387aa80a5e98a7f8f3836a026f0f
𝙰𝚞𝚐𝚞𝚜𝚝𝚒𝚗 <xaugustinx12@gmail.com>
2025-11-27 11:18:02 +0100

    Add files via upload

--- main.asm
+++ main.asm
@@ -1,23 +1,46 @@
 .global _start
 .intel_syntax noprefix
 
+.section .bss
+	buffer: .space 255
+
 .section .data
 msg:
 	.ascii "Meow Meow Meow\n"
 
-mango67musztardaTyNieJestesTuf:
-	.Byte 1
+enterKurwa:
+	.ascii "\n"
 
 .section .text
 
 _start:
-	mov rax, mango67musztardaTyNieJestesTuf
-	mov rdi, mango67musztardaTyNieJestesTuf
-	
+	mov rax, 1
+	mov rdi, 1
 	lea rsi, [rip + msg]
 	mov rdx, 15
 	syscall
 
+
+	mov rax, 0
+	mov rdi, 0
+	lea rsi, [rip + buffer]
+	mov rdx, 255
+	syscall
+
+
+	add byte ptr [rip + buffer], 1
+	#add byte ptr [rip + meowZmienna], 1
+
+	mov rax, 1
+	mov rdi, 1
+	lea rsi, [rip + buffer]
+	syscall
+
+	#jmp mango
+
+
 	mov rax, 60
 	mov rdi, 69
 	syscall	
+
+
